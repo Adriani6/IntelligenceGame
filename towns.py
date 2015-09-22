6ad4a8b4e2fc5ae23e2a3d61df734e928561bbdd
Adriani6 <adrian@adriani6.co.uk>
2015-09-22 21:38:26 +0100

    Added more classes Started GUI implementation
    Signed-off-by: Adriani6 <adrian@adriani6.co.uk>

--- towns.py
+++ towns.py
@@ -8,15 +8,9 @@
 # Copyright:   (c) Adrian 2015
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
-# Note: In Python 3 + Tkinter has been renamed to tkinter (lower case). When compiling on a machine with Python 2 or < make sure to change to a capital letter.
-# Import tkinter
-import tkinter
-# Define tkinter
-gui = tkinter.Tk()
 
 def main():
-    # Loop GUI to stay on screen as the program starts.
-    gui.mainloop()
+    pass
 
 if __name__ == '__main__':
     main()
